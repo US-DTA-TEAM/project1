2badfd43a7cabd8457f3b281f870156315335561
bkinaolga <bkinaolga@gmail.com>
2024-05-29 14:59:58 +0200

    modified by Olga

--- privateNACL.tf
+++ privateNACL.tf
@@ -1,7 +1,31 @@
+//create a private NACL
+resource "aws_network_acl" "terrajens_priv_nacl" {
+    vpc_id = aws_vpc.test_vpc.id
 
-/*
-create a private NACL
-inbound rule: from within VPC ONLY
+ egress {
+    protocol   = "tcp"
+    rule_no = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  tags = {
+    Name = "jerrajens_priv_nacl"
+  }
+}   
+  
+/*inbound rule: from within VPC ONLY
 outbound rule: to internet using all ports
 */
 
--- publicNACL.tf
+++ publicNACL.tf
@@ -1,7 +1,31 @@
 
-/*
-create a public NACL
-inbound rule: from internet using port 22 ONLY
+//create a public NACL
+resource "aws_network_acl" "terrajens_pub_nacl" {
+  vpc_id = aws_vpc.test_vpc.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 8080
+    to_port    = 8080
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  tags = {
+    Name = "jerrajens_pub_nacl"
+  }
+}
+/*inbound rule: from internet using port 22 ONLY
 outbound rule: to internet using all ports
 */
 
