cad448d03448d9ab5b0a1e2f8cfc94f01c04b393
bkinaolga <bkinaolga@gmail.com>
2024-05-29 19:51:03 +0200

    2nd edition by hotfix_olga

--- privateNACL.tf
+++ privateNACL.tf
@@ -3,21 +3,21 @@ resource "aws_network_acl" "terrajens_priv_nacl" {
     vpc_id = aws_vpc.test_vpc.id
 
  egress {
-    protocol   = "tcp"
+    protocol   = "all"
     rule_no = 200
     action     = "allow"
     cidr_block = "0.0.0.0/0"
-    from_port  = 22
-    to_port    = 22
+    from_port  = 0
+    to_port    = 0
   }
 
   ingress {
-    protocol   = "tcp"
+    protocol   = "all"
     rule_no = 100
     action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 22
-    to_port    = 22
+    cidr_block = "150.0.0.0/16"
+    from_port  = 0
+    to_port    = 0
   }
 
   tags = {
