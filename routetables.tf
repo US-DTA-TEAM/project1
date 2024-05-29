6a35ce34f138bda3252a9181280c9e88526583a2
kienfru <madokien0101@gmail.com>
2024-05-29 21:37:43 +0200

    modified by kien

--- routetables.tf
+++ routetables.tf
@@ -19,8 +19,13 @@ resource "aws_route_table" "public_rt" {
 resource "aws_route_table" "priv_rt" {
     vpc_id = aws_vpc.test_vpc.id
 
+    route  {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.terrajens_natgw.id
+    }
+
         tags = {
-            Name = "public_rt"
+            Name = "priv_rt"
         }
     }
   
